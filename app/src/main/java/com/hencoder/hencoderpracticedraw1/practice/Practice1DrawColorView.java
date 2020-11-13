87a6e1a2e6a02cdd25fbac9b3453e15a7a7c6703
Leo W <wuliang0306@gmail.com>
2020-11-13 08:46:59 -0800

    draw color and circle

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice1DrawColorView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice1DrawColorView.java
@@ -3,10 +3,10 @@ package com.hencoder.hencoderpracticedraw1.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import androidx.annotation.Nullable;
+
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
-
-import androidx.annotation.Nullable;
 
 public class Practice1DrawColorView extends View {
 
@@ -26,7 +26,6 @@ public class Practice1DrawColorView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-//        练习内容：使用 canvas.drawColor() 方法把 View 涂成黄色
-//        黄色： Color.YELLOW
+        canvas.drawColor(Color.YELLOW);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -3,10 +3,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import androidx.annotation.Nullable;
+
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+
+    Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -26,5 +31,24 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+
+        float radius = 190;
+        float horizontalOffset = 100;
+        canvas.drawCircle(horizontalOffset + radius, radius, radius, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        canvas.drawCircle(horizontalOffset + 3*radius + horizontalOffset/2, radius, radius, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(horizontalOffset + radius, 2*radius + horizontalOffset/2 + radius, radius, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(70);
+        canvas.drawCircle(horizontalOffset + 3*radius + horizontalOffset/2, 2*radius + horizontalOffset/2 + radius, radius, paint);
     }
 }
